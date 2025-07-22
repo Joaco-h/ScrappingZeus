f52e95d11c41b067713d5b3c89db0ef4a52856a1
Joaco <joacohb123@gmail.com>
2025-07-21 23:01:38 -0400

    Fix mac errors

--- configs/config_ml.py
+++ configs/config_ml.py
@@ -2,6 +2,7 @@ from utils.functions import Path, os
 from configs.settings import ML_DIR
 
 #MACHINE LEARNING
-ml_path = fr'{Path(ML_DIR) }\202407231124'
+# ml_path = fr'{Path(ML_DIR) }\202407231124'
+ml_path = os.path.join(ML_DIR, '202407231124')
 model_path = os.path.join(ml_path, 'model.h5')
 configs_path = os.path.join(ml_path, 'configs.yaml')
